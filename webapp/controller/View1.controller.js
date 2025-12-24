11f8001147dabf6042eb5bd0147a958243b0f0b3
praveen m <praveengouranga@gmail.com>
2025-12-24 07:27:47 +0000

    completed the object list control

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,23 +1,46 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/StandardListItem"
-], (Controller,StandardListItem) => {
+    "sap/m/ObjectListItem",
+     "sap/m/ObjectAttribute",
+     "sap/m/ObjectStatus",
+    //  "sap/ui/core/ValueState"
+    "sap/ui/core/library"
+], (Controller,ObjectListItem,ObjectAttribute,ObjectStatus,coreLibrary) => {
     "use strict";
 
     return Controller.extend("com.po.countdowntimer.controller.View1", {
         onInit() {
         },
         
-   onpress: function(){
+   onPressNewproduct: function(){
 
       // get input value
-      const input= this.getView().byId("input").getValue()
+      const sInput= this.getView().byId("input").getValue()
+    const sCategory = this.getView().byId("idcategory").getSelectedItem();
+    const sPrice = this.getView().byId("idprice").getValue()
+    const sReleaseDate =  this.getView().byId("idReleasedate").getDateValue()
+    const sDiscontinueDate =  this.getView().byId("idDiscontinueddate").getDateValue()
     //   adding the new item
 
-      this.getView().byId("idList").addItem(new StandardListItem({
-        title:input
+      this.getView().byId("idList").addItem(new ObjectListItem({
+        title:sInput,
+        number:sPrice,
+        numberUnit:"Eur",
+        attributes:[
+          new ObjectAttribute({ title:"category"   ,  text: sCategory.getText()}),
+          new ObjectAttribute({ title:"ReleaseDate"   ,  text:sReleaseDate}),
+        ],
+        firstStatus:
+          new ObjectStatus({ 
+            text : this._getAvailibilityText(sDiscontinueDate),
+            state : this._getAvailibilityState(sDiscontinueDate)
+
+             })
+        
       })
     );
+
+ 
     },
 
     onPressDelete: function(oEvent){
@@ -25,7 +48,26 @@ sap.ui.define([
       const oItem = oEvent.getParameter("listItem")
 
       this.getView().byId("idList").removeItem(oItem)
+    },
+
+    _getAvailibilityText:function(sDate){
+      const oDate = new Date(sDate);
+
+         return oDate > new Date() ?  "Available":"Unavailable";
+
+    },
+
+    _getAvailibilityState:function(sDate){
+
+      const ValueState = coreLibrary.ValueState;
+      const oDate = new Date(sDate);
+
+       return oDate > new Date() ?  ValueState.Success:ValueState.Error;
+
+
     }
+
+    
 
 });
 });
